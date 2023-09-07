b167de7b982c989c37c06379d9da9938dab83b98
Michael Tsaban <41857330+michaelts1@users.noreply.github.com>
2023-09-07 18:19:53 +0300

    Install tooling

--- compileIconsToBase64.js
+++ compileIconsToBase64.js
@@ -1,4 +1,4 @@
-const fs = require('fs/promises')
+import fs from 'fs/promises'
 
 const TYPE = 'low' // either 'low' or 'high'
 const filenames = ['archive-24.png', 'audio-24.png', 'avi-24.png', 'bmp-24.png', 'calc-24.png', 'document-24.png', 'flash-24.png', 'gif-24.png', 'image-24.png', 'impress-24.png', 'jpeg-24.png', 'mp3-24.png', 'mpeg-24.png', 'pdf-24.png', 'png-24.png', 'powerpoint-24.png', 'quicktime-24.png', 'sourcecode-24.png', 'spreadsheet-24.png', 'text-24.png', 'tiff-24.png', 'unknown-24.png', 'video-24.png', 'wmv-24.png', 'writer-24.png']
