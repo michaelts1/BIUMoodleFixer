3676b4fd85e2d4c20624b00649b76470a5e3b34b
Michael Tsaban <41857330+michaelts1@users.noreply.github.com>
2025-11-05 21:31:58 +0200

    Course List Revamp: If the UI is English, use the English course names

--- src/content/courseListRevamp.ts
+++ src/content/courseListRevamp.ts
@@ -8,19 +8,30 @@ export function courseListRevamp() {
 
 	document.body.classList.add(COURSE_LIST_REVAMP_CLASS)
 
+	const isHebrew = document.documentElement.lang === 'he'
+
 	const courseLinks = $m<HTMLElement>('.block-fcl__list__item--course a')
 	for (const courseLink of courseLinks) {
 		// The third out of the three nodes contains the name of the course
 		const courseTextNode = courseLink.childNodes[2]
 
+		const courseNameRaw = courseTextNode.textContent?.trim() ?? ''
+		const courseNameEnglishStart = courseNameRaw.search(/[A-z]/)
+		console.log(courseNameRaw, courseNameEnglishStart)
+		const courseName = courseNameEnglishStart === -1
+			? courseNameRaw
+			: isHebrew
+				? courseNameRaw.slice(0, courseNameEnglishStart)
+				: courseNameRaw.slice(courseNameEnglishStart)
+
+		const courseNumber = courseLink.title.substring(2) ?? '0'
+
 		const spanCourseName = document.createElement('span')
 		spanCourseName.classList.add('text-overflow-ellipsis')
-		spanCourseName.textContent = (courseTextNode.textContent ?? '')
-			.replace(/[A-z].*/, ' ') // Remove English name
-			.trim() // Remove extra whitespace
+		spanCourseName.textContent = courseName
 
 		const spanCourseNumber = document.createElement('span')
-		spanCourseNumber.textContent = courseLink.title.substring(2) ?? '0'
+		spanCourseNumber.textContent = courseNumber
 
 		courseLink.replaceChild(spanCourseName, courseTextNode)
 		courseLink.appendChild(spanCourseNumber)
